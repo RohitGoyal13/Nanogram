552bcc88304e393c77fb8f000bb12a1eeaebc9a1
Rohit Goyal <rohitgoyal4220@gmail.com>
2024-02-04 11:00:00 +0530

    Added DB connection and basic auth/user routes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
 import multer from "multer";
 
 const app = express();
@@ -16,6 +18,15 @@ app.use(cors(
         credentials: true,
     }
 ))
+
+
+
+
+
+
+app.use("/server/auth", authRoutes);
+app.use("/server/users", userRoutes);
+
 
 
 app.listen(8800, () => {

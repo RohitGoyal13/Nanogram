2c16baf2f203cb7b6712cac1565c9f3975060073
Rohit Goyal <rohitgoyal4220@gmail.com>
2025-04-15 01:50:47 +0530

    Initial Commit

---
+++ client/social/src/context/authContext.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(() => {
+    try {
+      const storedUser = localStorage.getItem("user");
+      return storedUser ? JSON.parse(storedUser) : null;
+    } catch (err) {
+      console.error("Failed to parse user from localStorage", err);
+      return null;
+    }
+  });  
+
+  const login = async (inputs) => {
+    //TO DO
+    const res = await axios.post("http://localhost:8800/server/auth/login",inputs,{
+        withCredentials: true,
+  });
+    setCurrentUser(res.data)
+  
+};
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ server/index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/post.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import multer from "multer";
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", true);
+    next();
+});
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        credentials: true,
+    }
+))
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../client/social/public/upload')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname);
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
+app.post("/server/upload", upload.single("file"),(req, res) =>{
+    const file= req.file;
+  if (!file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+    res.status(200).json(file.filename);
+})
+app.use("/server/auth", authRoutes);
+app.use("/server/users", userRoutes);
+app.use("/server/posts", postRoutes);
+app.use("/server/comments", commentRoutes);
+app.use("/server/likes", likeRoutes);
+
+app.listen(8800, () => {
+    console.log("Server is running on port 8800")
+});
